18ac2cd275b20617075f934f937ff96d8bb4aeea
Olga <barsolet@mail.ru>
2018-09-24 23:47:43 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' '),
+      a = arr[0],
+      b = arr[3] + arr[4],
+      c = arr[7] + arr[8];
+
+    var d = b * b - 4 * a * c;
+
+    var x1 = Math.round(( -b + Math.sqrt(d))/(2 * a));
+
+    var x2 = Math.round(( -b - Math.sqrt(d))/(2 * a));
+
+
+    var results = [x1, x2];
+
+    function compareNumeric(x1, x2) {
+        if (x1 > x2) return 1;
+        if (x1 < x2) return -1;
+    }
+
+    results.sort(compareNumeric);
+
+    return results;
 }
